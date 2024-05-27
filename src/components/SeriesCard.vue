badd9d6991d0a2e1a3753595e3e6f0656c11493d
giacomo-spadoni <giaky.spado@gmail.com>
2024-05-27 10:21:17 +0200

    mileston 1 complited (new exercise)

---
+++ src/components/SeriesCard.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  props: ['fumetto'],
+  methods: {
+    getImage(nomefile) {
+      return new URL(`${nomefile}`, import.meta.url);
+    },
+  },
+};
+</script>
+<template>
+  <div class="card">
+    <div class="img-container">
+      <img :src="getImage(fumetto.thumb)" alt="" />
+    </div>
+    <div class="title">{{ fumetto.series }}</div>
+  </div>
+</template>
+<style scoped>
+.card {
+  width: 15%;
+  margin-bottom: 2rem;
+}
+
+.img-container img {
+  width: 100%;
+  aspect-ratio: 1;
+  object-fit: cover;
+}
+
+.title {
+  font-size: 16px;
+}
+</style>
